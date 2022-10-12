9198bbf1e91538bc7a3fa07cfeee44adf50b8731
afiq sam <afiqsam71@gmail.com>
2022-10-12 23:40:39 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,199 @@
+// console.log('Hello World')
+    // alert('Helo Javascript')
+
+
+
+
+    //if, else if, else
+
+    // if(condition){
+    //     statement
+    // } else if(anotherCondition){
+    //     statement
+    // } else if (yetanotherCondition){
+    //     statement
+    // } else {
+    //     statement
+    // }
+
+    // let number = 13;
+
+    // if(number < 10) {
+    //     console.log('Excellent')
+    // } else {
+    //     console.log('Great')
+    // }
+
+
+    // if number is between 1 to 4: "excellent"
+    // if number is between 5 to 10: "Great"
+    // otherwise: 'Good Job'
+
+
+    // let number = 2;
+
+    // if(number >= 1 && number <= 4) {
+    //     console.log('Excellent')
+    // } else if(number >= 5 && number <= 10){
+    //     console.log('Great')
+    // }else {
+    //     console.log('Good Job')
+    // }
+
+
+
+
+    // What is var, let, const?
+
+    //var & let = kita boleh replace the value
+    // const = tak boleh replace
+
+    // const x = 100;
+
+    // x = 1000
+
+    // console.log(x)
+
+
+
+
+    //var = global
+    // let & const = tak bersifat global
+
+
+    // if(true) {
+
+    //     var x = 'I love Javascript';
+    //     let y = 'I love programming';
+    //     const z = 'I love Coding'
+
+    //     console.log(x)
+    //     console.log(y)
+    //     console.log(z)
+
+    // }
+
+    // console.log('////////////')
+
+    //     console.log(x)
+    //     console.log(y)
+    //     console.log(z)
+
+
+
+
+
+
+
+
+
+
+    //Primitive types
+
+    //String
+
+    // let name = 'Afiq'
+
+
+    //Numbers
+
+    // let number = 10;
+
+    //Boolean
+
+    // let isApproved = true;
+    // let notApproved = false;
+
+
+    //Undefined
+
+    // let person = undefined;
+
+    //null
+
+    // let color = null;
+
+    //null = 0
+
+
+    // let color = {
+    //     warnaKegemaran: 'blue'
+    // }
+
+    // console.log(color)
+
+    // let object = {
+    //     name: 'Afiq',
+    //     age: 26,
+    // }
+
+    // dot notation
+    
+    // console.log(object)
+
+   
+    
+    //bracket notation
+
+    // let selection = 'age';
+
+    // object[selection] = '27'
+
+    // console.log(object)
+
+
+
+
+
+
+    // Array
+
+    // let color = ['green', 'blue', 'black']
+
+    // color[0] = 'white'
+
+    // console.log(color)
+
+
+
+
+
+
+
+
+
+    //Function
+
+    //performing the task
+
+    // function name(parameter) {
+    //     statement
+    // }
+
+    // name(argument)
+
+
+    // function display(firstname, lastname){
+    //     console.log('Hello ' + firstname + ' ' + 'bin' + ' ' + lastname)
+    // }
+
+    // display('Ali', 'Abu')
+
+
+
+    // function square(number) {
+    //     console.log(number*number)
+    // }
+
+    // square(2)
+
+    // let var_name = 'Ali';
+    // let var_name2 = 'belum'
+
+    // console.log('hello ' + var_name + ' dah makan ke? ' + var_name2)
+
+
+    function tekan() {
+        document.getElementById('show').innerHTML = "Hello Javascript"
+    }
+
